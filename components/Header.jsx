9c762b67faf7c218c54eef4e83ef253227acd33d
Joao-Terencio <jh.terencio@gmail.com>
2024-07-26 00:54:20 -0300

    Start 21:50

---
+++ components/Header.jsx
@@ -0,0 +1,34 @@
+import Link from "next/link"
+import { Button } from "./ui/button"
+
+// Componemts
+import Nav from "./Nav";
+
+const Header = () => {
+  return (
+    <header className="py-8 xl:py-10 text-white">
+      <div className="container max-auto flex justify-between">
+        {/* Logo */}
+        <Link href="/">
+          <h1 className="text-2xl font-semibold">
+            Terêncio<span className="text-accent">.</span>
+          </h1>
+        </Link>
+
+        {/* Desktop Nav */}
+        <div className="hidden xl:flex items-center gap-8">
+          <Nav/>
+          <Link href="/contact">
+            <Button>Hire me</Button>
+          </Link>
+        </div>
+
+        {/* Mobile nav */}
+        <div className="xl:hidden">Mobile nav</div>
+
+      </div>
+    </header>
+  )
+}
+
+export default Header
