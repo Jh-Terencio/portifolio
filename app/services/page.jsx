526443a0bc0f7f7fbebd0695acaec4f567a76727
Joao-Terencio <jh.terencio@gmail.com>
2024-07-30 01:09:46 -0300

    Finishing works page

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -39,7 +39,7 @@ const Services = () => {
           initial={{ opacity: 0 }}
           animate={{
             opacity: 1,
-            transition: {delay: 2.4, duration: 0.4, ease: "easeIn"},
+            transition: {delay: 1.8, duration: 0.4, ease: "easeIn"},
           }}
           className="grid grid-cols-1 md:grid-cols-2 gap-[30px]"
         >
